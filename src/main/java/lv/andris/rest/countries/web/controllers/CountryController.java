8017ced3cbd2df0bf7c73c185c6af264194e7899
Darxney <Tresutinsandris@gmail.com>
2021-11-02 01:09:33 +0200

    //

--- src/main/java/lv/andris/rest/countries/web/controllers/CountryController.java
+++ src/main/java/lv/andris/rest/countries/web/controllers/CountryController.java
@@ -9,7 +9,6 @@ import lv.andris.rest.countries.web.controllers.CountryController_classes.Messag
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
